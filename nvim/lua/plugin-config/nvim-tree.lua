77bad4c5325c81e86fa0d045a0f7543d89b8f31e
scmeek <sean@seanmeek.com>
2022-05-05 14:26:41 -0600

    nvim - nvim-tree close if last buffer

--- nvim/lua/plugin-config/nvim-tree.lua
+++ nvim/lua/plugin-config/nvim-tree.lua
@@ -2,3 +2,7 @@ require('nvim-tree').setup({})
 
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
 
+vim.api.nvim_create_autocmd('BufEnter', {
+    command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+    nested = true,
+})
