4e84c86cec27393de3947ba3f8c02ad00edf181f
scmeek <sean@seanmeek.com>
2022-05-04 20:13:07 -0600

    nvim - add autocompletion

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,8 +14,15 @@ return require('packer').startup({
     	as = 'dracula'
     	}
 
+		-- Language server protocol
     use {
     	'neovim/nvim-lspconfig',
+    	requires = {
+					'hrsh7th/nvim-cmp',						-- Autocompletion plugin
+					'hrsh7th/cmp-nvim-lsp',				-- LSP source for nvim-cmp
+					'saadparwaiz1/cmp_luasnip',		-- Snippets source for nvim-cmp
+					'L3MON4D3/LuaSnip'						-- Snippets plugin
+    	},
     	config = get_config('nvim-lspconfig')
     	}
 
