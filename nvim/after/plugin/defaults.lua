e459ded395484474d51b148c777155b0dfd70c22
scmeek <sean@seanmeek.com>
2022-12-19 18:31:38 -0700

    nvim - Convert previous init.lua, options.lua, and keymaps.lua to new defaults.lua

---
+++ nvim/after/plugin/defaults.lua
@@ -0,0 +1,191 @@
+--                 °#@@@@@#.       *@@@@@@*           °°°°°.
+--               O@@@@o.°#@@#   .@@@@@@@@@@@.       °@@@@@@#
+--              #@@@@o    @@@  *@@°  o@@@@@@@      *@@@@@@@
+--              @@@@@@.  o@@#  @@     *@@@@@@@    o@@@@@@@@
+--              @@@@@@@#      #@*     @@@@@@@@o  #@@@@@@@@.
+--              @@@@@@@@@*    @@@.    @#@@@@@@@ @@o@@@@@@@
+--               @@@@@@@@@#   *@@@@* @@ o@@@@@@@@ O@@@@@@°
+--                *@@@@@@@@*   o@@*  @@  @@@@@@@  @@@@@@@
+--        .@@@O     @@@@@@@@        @@*  @@@@@@  O@@@@@@o  #
+--        @@@@o      @@@@@@@       #@@   #@@@@   @@@@@@@  @@
+--       O@@@@       .@@@@@°*#   *@@@    *@@@    @@@@@@@ @@
+--       °@@@@       O@@@@@ @@@@@@@@°     @@     o@@@@@@@@.
+--        #@@@*     *@@@@O   @@@@@@.      #       *@@@@@o
+--         °@@@@@#@@@@@o       °°
+--            °o##Oo.
+--
+--      Personal vim configuration of Sean Meek <sean@seanmeek.com>
+
+----------------------------------------------------------------------------
+-- General
+----------------------------------------------------------------------------
+
+-- :h <option> for details
+-- :options lists all
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.copyindent = true
+vim.opt.preserveindent = true
+vim.opt.autoindent = true
+
+vim.opt.compatible = false
+vim.opt.showmatch = true
+vim.opt.exrc = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.hlsearch = false
+vim.opt.errorbells = false
+vim.opt.wrap = false
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undo-history'
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.colorcolumn = '80'
+vim.opt.laststatus = 3
+vim.opt.cmdheight = 0
+vim.opt.spell = true
+vim.opt.signcolumn = 'auto'
+vim.opt.showmode = false
+vim.opt.completeopt = { 'menuone', 'noinsert', 'noselect' }
+vim.opt.wildmode = { 'longest', 'list', 'full' }
+vim.opt.wildmenu = true
+vim.opt.inccommand = 'split' -- Live substitution
+vim.opt.showbreak = '↪'
+vim.opt.list = true
+vim.opt.listchars = { tab = '| ', trail = '·' }
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.confirm = true
+vim.opt.backup = true
+vim.opt.backupdir = os.getenv('HOME') .. '/.vim/backup'
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.timeoutlen = 500
+vim.opt.updatetime = 50
+
+vim.opt.termguicolors = true
+
+-- More natural split opening
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+----------------------------------------------------------------------------
+-- Keymaps
+----------------------------------------------------------------------------
+
+-- Already taken care of in init.lua
+--vim.g.mapleader = ' '
+--vim.g.maplocalleader = ' '
+
+-- Save buffer
+--vim.keymap.set('', '<leader>s', ':w<CR>')
+
+-- Select all
+vim.keymap.set('n', '<leader>a', 'ggVG')
+
+-- Easier split navigations
+vim.keymap.set('n', '<C-J>', '<C-W><C-J>')
+vim.keymap.set('n', '<C-K>', '<C-W><C-K>')
+vim.keymap.set('n', '<C-L>', '<C-W><C-L>')
+vim.keymap.set('n', '<C-H>', '<C-W><C-H>')
+
+-- Easier splits handling
+vim.keymap.set('n', '<leader>|', '<C-W>v', { silent = true })
+vim.keymap.set('n', '<leader>-', '<C-W>s', { silent = true })
+vim.keymap.set('n', '<leader>>', '<C-W>>', { silent = true })
+vim.keymap.set('n', '<leader><', '<C-W><', { silent = true })
+
+-- Reselect visual selection after indenting
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Maintain the cursor position when yanking a visual selection
+--  http://ddrscott.github.io/blog/2016/yank-without-jank/
+vim.keymap.set('v', 'y', 'myy`y')
+vim.keymap.set('v', 'Y', 'myY`y')
+
+-- Reselect pasted text
+vim.keymap.set('n', 'gp', '`[v`]')
+
+-- When text is wrapped, move by terminal rows, not lines, unless a count is provided
+vim.keymap.set('', 'j', function()
+    return vim.v.count == 0 and 'gj' or 'j'
+end, { silent = true, expr = true })
+vim.keymap.set('', 'k', function()
+    return vim.v.count == 0 and 'gk' or 'k'
+end, { silent = true, expr = true })
+
+-- Make Y behave like the other capitals
+vim.keymap.set('n', 'Y', 'y$')
+
+-- Move line up
+vim.keymap.set('n', '<leader>mk', ':m -2<CR>')
+-- Move line down
+vim.keymap.set('n', '<leader>mj', ':m +1<CR>')
+
+-- Diff with clipboard
+--  https://www.reddit.com/r/neovim/comments/sg919r/diff_with_clipboard/huy72t0/?utm_source=reddit&utm_medium=web2x&context=3
+vim.keymap.set('n', 'cp', function()
+    local ftype = vim.api.nvim_eval('&filetype')
+    vim.cmd(string.format(
+        [[
+    	vsplit
+    	enew
+    	normal! P
+    	setlocal buftype=nowrite
+    	set filetype=%s
+    	diffthis
+    	bprevious
+    	execute "normal! \<C-w>\<C-w>"
+    	diffthis
+  	]],
+        ftype
+    ))
+end)
+
+----------------------------------------------------------------------------
+-- Miscellaneous
+----------------------------------------------------------------------------
+
+vim.g.custom_filetree_offset = 50
+
+----------------------------------------------------------------------------
+-- Autogroups
+----------------------------------------------------------------------------
+
+
+local local_group = vim.api.nvim_create_augroup('Local', { clear = true })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Temporarily highlight text on yank',
+    group = local_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({ higroup = 'IncSearch' })
+    end,
+})
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    desc = 'Trim whitespace on save',
+    group = local_group,
+    pattern = '*',
+    command = ':%s/\\s\\+$//e',
+})
+
+vim.api.nvim_create_autocmd('VimLeave', {
+    desc = 'Set cursor to blinking horizontal when leaving neovim',
+    group = local_group,
+    pattern = '*',
+    command = 'set guicursor=a:hor25-blinkon10',
+})
+
+
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
