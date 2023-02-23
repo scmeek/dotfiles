322f4b58ffc239fddc8d97f57e678b5e53e66a9b
Sean Meek <sean@seanmeek.com>
2023-02-23 13:33:56 -0700

    nvim - vim-tmux-navigator, fix split navigations keymaps

--- nvim/lua/custom/plugins/vim-tmux-navigator.lua
+++ nvim/lua/custom/plugins/vim-tmux-navigator.lua
@@ -3,7 +3,9 @@ return {
 	keys = {
 		{ '<leader>|', '<C-W>v', desc = 'Split [|] vertical' },
 		{ '<leader>-', '<C-W>s', desc = 'Split [-] horizontal' },
-		{ '<leader>>', '<C-W>>', desc = 'Go to [>] next split' },
-		{ '<leader><', '<C-W><', desc = 'Go to [<] previous split' },
+		{ '<C-h>', '<C-W>h', desc = 'Go to previous split' },
+		{ '<C-j>', '<C-W>j', desc = 'Go down a split' },
+		{ '<C-k>', '<C-W>k', desc = 'Go up a split' },
+		{ '<C-l>', '<C-W>l', desc = 'Go to next split' },
 	}
 }
