23d70c788f7e3a68a00579559bfc07f096e0c832
Sean Meek <sean@seanmeek.com>
2023-02-23 13:33:31 -0700

    nvim - neo-tree, include diagnostic signs and remove options set to default

--- nvim/lua/custom/plugins/neo-tree.lua
+++ nvim/lua/custom/plugins/neo-tree.lua
@@ -12,24 +12,18 @@ return {
     config = function()
         vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
+        vim.fn.sign_define("DiagnosticSignError",
+            { text = " ", texthl = "DiagnosticSignError" })
+        vim.fn.sign_define("DiagnosticSignWarn",
+            { text = " ", texthl = "DiagnosticSignWarn" })
+        vim.fn.sign_define("DiagnosticSignInfo",
+            { text = " ", texthl = "DiagnosticSignInfo" })
+        vim.fn.sign_define("DiagnosticSignHint",
+            { text = "", texthl = "DiagnosticSignHint" })
+
         require('neo-tree').setup({
             close_if_last_window = true,
             sort_case_insensitive = true,
-            git_status = {
-            symbols = {
-              -- Change type
-              added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-              modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-              deleted   = "✖",-- this can only be used in the git_status source
-              renamed   = "",-- this can only be used in the git_status source
-              -- Status type
-              untracked = "",
-              ignored   = "",
-              unstaged  = "",
-              staged    = "",
-              conflict  = "",
-            }
-          },
         })
     end,
 }
