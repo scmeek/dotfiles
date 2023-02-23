e758aa3ef7452210f4c824e206de0dd6b0f6510c
Sean Meek <sean@seanmeek.com>
2023-02-22 19:05:39 -0700

    nvim - remove duplicate highlight on yank autocmd (in kickstarter.nvim init.lua)

--- nvim/after/plugin/autocmds.lua
+++ nvim/after/plugin/autocmds.lua
@@ -1,14 +1,5 @@
 local local_group = vim.api.nvim_create_augroup('Local', { clear = true })
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Temporarily highlight text on yank',
-    group = local_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({ higroup = 'IncSearch' })
-    end,
-})
-
 vim.api.nvim_create_autocmd('BufWritePre', {
     desc = 'Trim whitespace on save',
     group = local_group,
