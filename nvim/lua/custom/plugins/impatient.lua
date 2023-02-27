fc329fd3b68b71350db92be2f25cc4ecdd5df2fd
Sean Meek <sean@seanmeek.com>
2023-02-27 12:00:01 -0700

    nvim - lazyvim, prefer lazyvim setup hooks over manual setup call

--- nvim/lua/custom/plugins/impatient.lua
+++ nvim/lua/custom/plugins/impatient.lua
@@ -1,7 +1,4 @@
 -- Improve startup time by caching lua modules
 return {
 	'lewis6991/impatient.nvim',
-	config = function()
-		require('impatient')
-	end,
 }
--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -1,34 +1,32 @@
 return {
     'nvim-lualine/lualine.nvim',
-    config = function()
-        require('lualine').setup {
-            options = {
-                icons_enabled = true,
-                theme = 'auto',
-                component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
-                fmt = string.lower,
-            },
-            sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = {
-                    {
-                        'filename',
-                        newfile_status = true,
-                        path = 1,
-                        symbols = {
-                            modified = '*',
-                            readonly = '[-]',
-                            unnamed = 'untitled',
-                            newfile = 'untitled',
-                        },
+    opts = {
+        options = {
+            icons_enabled = true,
+            theme = 'auto',
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
+            fmt = string.lower,
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff', 'diagnostics' },
+            lualine_c = {
+                {
+                    'filename',
+                    newfile_status = true,
+                    path = 1,
+                    symbols = {
+                        modified = '*',
+                        readonly = '[-]',
+                        unnamed = 'untitled',
+                        newfile = 'untitled',
                     },
                 },
-                lualine_x = { 'encoding', 'fileformat', 'filetype' },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' }
             },
-        }
-    end,
+            lualine_x = { 'encoding', 'fileformat', 'filetype' },
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' }
+        },
+    },
 }
