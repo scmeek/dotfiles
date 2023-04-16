b2858415a02cee876911bc6b3febc678a7120892
scmeek <sean@seanmeek.com>
2023-04-16 12:49:00 -0600

    nvim - add shell formatter

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -1,6 +1,30 @@
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
 return {
 	'jose-elias-alvarez/null-ls.nvim',
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 	},
+	config = function()
+		-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+		require('null-ls').setup({
+			sources = {
+				require('null-ls').builtins.formatting.shfmt,
+			},
+			-- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
+			-- you can reuse a shared lspconfig on_attach callback here
+			on_attach = function(client, bufnr)
+				if client.supports_method('textDocument/formatting') then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd('BufWritePre', {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
