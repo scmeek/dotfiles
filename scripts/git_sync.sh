db8ceed3c9dd278e52bee80fd9f3918d5ad1e3c2
scmeek <sean@seanmeek.com>
2023-04-19 10:47:21 -0600

    script - format git_sync

--- scripts/git_sync.sh
+++ scripts/git_sync.sh
@@ -2,31 +2,30 @@
 #
 # Sync a git remote with remote
 
-set -euo pipefail  # exit on error, error on unset vars, don't mask errors
+set -euo pipefail # exit on error, error on unset vars, don't mask errors
 
-usage()
-{
-   echo ""
-   echo "Usage: $0 GIT_DIR"
-   echo -e "\tGIT_DIR: The git directory to sync"
-   exit 1
+usage() {
+	echo ""
+	echo "Usage: $0 GIT_DIR"
+	echo -e "\tGIT_DIR: The git directory to sync"
+	exit 1
 }
 
 if [[ $# -ne 1 ]]; then
-  usage
+	usage
 fi
 
 echo ""
 echo "$(date)"
 
-GIT_DIR="${1/#\~/$HOME}"  # Bash parameter expansion
+GIT_DIR="${1/#\~/$HOME}" # Bash parameter expansion
 
 [[ ! -d "${GIT_DIR}" ]] && echo "💀 ${GIT_DIR} does not exist"
 
 cd "${GIT_DIR}"
 
 if [[ ! "$(git rev-parse --is-inside-work-tree 2>/dev/null)" ]]; then
-  echo "💀 ${GIT_DIR} is not a git repo"
+	echo "💀 ${GIT_DIR} is not a git repo"
 fi
 
 # stash changes
@@ -35,7 +34,7 @@ CHANGED_FILES=$(git diff --no-ext-diff HEAD)
 git restore --staged .
 
 if [[ "${CHANGED_FILES}" ]]; then
-  git stash --include-untracked
+	git stash --include-untracked
 fi
 
 # get latest
@@ -43,17 +42,15 @@ git pull --rebase
 
 # unstash changes
 if [[ "${CHANGED_FILES}" ]]; then
-  git stash pop
+	git stash pop
 
-  git add .
+	git add .
 
-  git commit -m "Automated update"
+	git commit -m "Automated update"
 
-  git push
+	git push
 fi
-
 
 echo "✅ Synced ${GIT_DIR}"
 echo "-----------------------"
 exit 0
-
