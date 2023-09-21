7c56bddecb3a2386e7de35946884290cb2f7ceb1
Sean Meek <sean@seanmeek.com>
2023-09-21 13:03:28 -0600

    nvim - null-ls, remove redundant format autocmd

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -14,20 +14,6 @@ return {
 				null_ls.builtins.formatting.shfmt,
 				null_ls.builtins.formatting.eslint_d,
 			},
-			-- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
-			-- you can reuse a shared lspconfig on_attach callback here
-			on_attach = function(client, bufnr)
-				if client.supports_method('textDocument/formatting') then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd('BufWritePre', {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
-						end,
-					})
-				end
-			end,
 		})
 	end,
 }
