cfa19531a025390c5f8702c890dc77a2c27f6969
Sean Meek <sean@seanmeek.com>
2023-09-21 13:01:29 -0600

    nvim - null-ls, add eslint_d

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -11,8 +11,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.completion.spell,
-
 				null_ls.builtins.formatting.shfmt,
+				null_ls.builtins.formatting.eslint_d,
 			},
 			-- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
 			-- you can reuse a shared lspconfig on_attach callback here
