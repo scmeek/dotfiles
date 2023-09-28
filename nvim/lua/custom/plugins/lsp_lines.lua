07df4ef278e62821911f5e8434032e68ab2f5069
Sean Meek <sean@seanmeek.com>
2023-09-28 12:51:34 -0600

    nvim - lsp_lines, fix function

--- nvim/lua/custom/plugins/lsp_lines.lua
+++ nvim/lua/custom/plugins/lsp_lines.lua
@@ -1,7 +1,7 @@
 return {
   'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
   keys = {
-    { '<leader>l', ':lua require("lsp_lines").toggle()<CR>', desc = 'Toggle [L]sp lines' },
+    { '<leader>l', function() require('lsp_lines').toggle() end, desc = 'Toggle [L]SP lines' },
   },
   config = function()
     require('lsp_lines').setup()
