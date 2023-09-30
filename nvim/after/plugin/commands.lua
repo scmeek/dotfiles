8031bb6743c446bfcb62901bb29dfc0b18b0ee5d
scmeek <sean@seanmeek.com>
2023-09-30 14:51:50 -0600

    nvim - replace diagnostic float

--- nvim/after/plugin/commands.lua
+++ nvim/after/plugin/commands.lua
@@ -1,3 +1,5 @@
+local constants = require('constants')
+
 local local_group = vim.api.nvim_create_augroup('Local', { clear = true })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
@@ -7,19 +9,37 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     command = ':%s/\\s\\+$//e',
 })
 
-vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-    callback = function()
-        vim.diagnostic.open_float({
-            scope = 'line',
-            show_header = false,
-            border = 'single',
-            severity_sort = true,
-        })
-    end,
-})
-
 vim.api.nvim_create_user_command('WQ', 'wq', {})
 vim.api.nvim_create_user_command('Wq', 'wq', {})
 vim.api.nvim_create_user_command('W', 'w', {})
 vim.api.nvim_create_user_command('Qa', 'qa', {})
 vim.api.nvim_create_user_command('Q', 'q', {})
+
+function OpenDiagnosticIfNoFloat()
+    for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+        if vim.api.nvim_win_get_config(winid).zindex then
+            return
+        end
+    end
+    vim.diagnostic.open_float({
+        scope = 'line',
+        show_header = false,
+        border = constants.border_style,
+        severity_sort = true,
+        focusable = false,
+        close_events = {
+            'CursorMoved',
+            'CursorMovedI',
+            'BufHidden',
+            'InsertCharPre',
+            'WinLeave',
+        },
+    })
+end
+
+vim.api.nvim_create_augroup('lsp_diagnostics_hold', { clear = true })
+vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+    pattern = '*',
+    callback = OpenDiagnosticIfNoFloat,
+    group = 'lsp_diagnostics_hold',
+})
