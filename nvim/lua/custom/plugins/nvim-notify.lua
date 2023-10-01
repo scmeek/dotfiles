a90272e53e78d6d36b9ace8bdf8247b25aed92fd
scmeek <sean@seanmeek.com>
2023-09-30 20:44:09 -0600

    nvim - configure nvim-notify

--- nvim/lua/custom/plugins/nvim-notify.lua
+++ nvim/lua/custom/plugins/nvim-notify.lua
@@ -1,12 +1,23 @@
+local icons = require('icons')
+
 return {
-    'rcarriga/nvim-notify',
-    config = function()
-        require('notify').setup({
-            render = 'compact',
-            stages = 'fade',
-            timeout = 2500,
-            background_colour = '#000000',
-        })
-        vim.notify = require('notify')
-    end,
+  'rcarriga/nvim-notify',
+  config = function()
+    local stages_util = require('notify.stages.util')
+
+    require('notify').setup({
+      fps = 120,
+      icons = {
+        DEBUG = icons.dap.log_point,
+        ERROR = icons.diagnostics.Error,
+        INFO = icons.diagnostics.Info,
+        TRACE = icons.git.Modified,
+        WARN = icons.diagnostics.Warn,
+      },
+      render = 'minimal',
+      stages = 'static',
+      timeout = 2000,
+    })
+    vim.notify = require('notify')
+  end,
 }
