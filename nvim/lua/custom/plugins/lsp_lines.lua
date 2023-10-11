d8a41ac035d63e4e6b0a8407dbe244e4554654a6
Sean Meek <sean@seanmeek.com>
2023-10-11 17:16:51 -0600

    nvim - add lsp_lines

---
+++ nvim/lua/custom/plugins/lsp_lines.lua
@@ -0,0 +1,17 @@
+return {
+  'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+  lazy = false,
+  keys = {
+    { '<leader>l', function() require('lsp_lines').toggle() end, desc = 'Toggle [L]SP lines' },
+  },
+  config = function()
+    require('lsp_lines').setup()
+    vim.diagnostic.config({
+      virtual_text = false,
+      virtual_lines = {
+        only_current_line = true,
+        highlight_whole_line = false,
+      },
+    })
+  end,
+}
