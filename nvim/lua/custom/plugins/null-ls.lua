e04eaa59e7ca5426feed716f39c2b12ce4e46e54
Sean Meek <sean@seanmeek.com>
2023-10-13 13:55:45 -0600

    nvim - shfmt and shellcheck for zsh files

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -4,12 +4,14 @@ return {
 		'nvim-lua/plenary.nvim',
 	},
 	config = function()
+		local shell_filetypes = { 'sh', 'zsh' }
 		-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 		local null_ls = require('null-ls')
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.completion.spell,
-				null_ls.builtins.formatting.shfmt,
+				null_ls.builtins.diagnostics.shellcheck.with({ filetypes = shell_filetypes }),
+				null_ls.builtins.formatting.shfmt.with({ filetypes = shell_filetypes }),
 				null_ls.builtins.formatting.eslint_d,
 			},
 		})
