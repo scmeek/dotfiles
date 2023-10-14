47ab6b561a084552f0533db5f4bb7b5a0e08561b
scmeek <sean@seanmeek.com>
2023-10-14 16:59:08 -0600

    nvim - add gt and gT keymaps (next/previous buffer)

--- nvim/after/plugin/keymaps.lua
+++ nvim/after/plugin/keymaps.lua
@@ -29,6 +29,8 @@ vim.keymap.set('i', '<M-BS>', '<Esc>cvb', { desc = "Backspace entire word" })
 -- Use <Tab> to cycle through buffers in tab
 vim.keymap.set('n', '<Tab>', ':bn<CR>', { desc = "Move to next buffer", silent = true })
 vim.keymap.set('n', '<S-Tab>', ':bp<CR>', { desc = "Move to previous buffer", silent = true });
+vim.keymap.set('n', 'gt', ':bn<CR>', { desc = "Move to next buffer", silent = true })
+vim.keymap.set('n', 'gT', ':bp<CR>', { desc = "Move to previous buffer", silent = true });
 
 vim.keymap.set({ 'n', 'v' }, 'H', '^', { desc = "Move to start of line" })
 vim.keymap.set({ 'n', 'v' }, 'L', '$', { desc = "Move to end of line" });
