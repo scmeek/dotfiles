dcc15bca14afeba2e76ed4f6598cd8d92c9a7f0c
scmeek <sean@seanmeek.com>
2023-10-19 00:34:19 -0600

    nvim - lastplace, fix

--- nvim/lua/custom/plugins/lastplace.lua
+++ nvim/lua/custom/plugins/lastplace.lua
@@ -1,11 +1,8 @@
 return {
     'ethanholz/nvim-lastplace',
-    config = function()
-        lastplace_ignore_buftype = { 'quickfix', 'nofile', 'help' }
-        lastplace_ignore_filetype = { 'gitcommit', 'gitrebase' }
-        lastplace_open_folds = true
-        require('which-key').register({
-            ['<Leader>q'] = { name = 'Lastplace', _ = 'which_key_ignore' },
-        })
-    end,
+    opts = {
+        lastplace_ignore_buftype = { 'quickfix', 'nofile', 'help' },
+        lastplace_ignore_filetype = { 'gitcommit', 'gitrebase' },
+        lastplace_open_folds = true,
+    },
 }
