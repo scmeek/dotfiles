40fe7bd139d66701043def12b913bd02ec9f16e6
scmeek <sean@seanmeek.com>
2023-10-19 00:51:11 -0600

    nvim - persistence, fix

--- nvim/lua/custom/plugins/persistence.lua
+++ nvim/lua/custom/plugins/persistence.lua
@@ -1,10 +1,26 @@
+require('which-key').register({
+  ['<Leader>q'] = { name = 'Persistence', _ = 'which_key_ignore' },
+})
+
 return {
   'folke/persistence.nvim',
   event = 'BufReadPre',
-  opts = { options = { 'buffers', 'curdir', 'tabpages', 'winsize', 'help', 'globals', 'skiprtp' } },
   keys = {
     { '<leader>qs', function() require('persistence').load() end,                desc = 'Restore [S]ession' },
     { '<leader>ql', function() require('persistence').load({ last = true }) end, desc = 'Restore [L]ast Session' },
     { '<leader>qd', function() require('persistence').stop() end,                desc = "[D]on't Save Current Session" },
   },
+  config = function()
+    require('persistence').setup({
+      options = {
+        'buffers',
+        'curdir',
+        'tabpages',
+        'winsize',
+        'help',
+        'globals',
+        'skiprtp'
+      },
+    })
+  end,
 }
