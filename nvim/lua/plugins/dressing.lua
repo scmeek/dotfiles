46979e599c7a9cf8b19c2dfa97230e1390581337
scmeek <sean@seanmeek.com>
2023-11-04 13:57:29 -0600

    nvim - add dressing

---
+++ nvim/lua/plugins/dressing.lua
@@ -0,0 +1,22 @@
+return {
+  'stevearc/dressing.nvim',
+  event = 'VeryLazy',
+  opts = {
+    input = {
+      enabled = true,
+      default_prompt = '➤ ',
+      win_options = {
+        winblend = 0,
+      },
+    },
+    select = {
+      enabled = true,
+      backend = { 'telescope', 'builtin' },
+      builtin = {
+        win_options = {
+          winblend = 0,
+        },
+      },
+    },
+  },
+}
