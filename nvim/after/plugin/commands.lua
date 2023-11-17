360145fa07fe242ce04e503d999af514d41ff3d6
Sean Meek <sean@seanmeek.com>
2023-11-17 11:09:55 -0700

    nvim - add command to copy messages

--- nvim/after/plugin/commands.lua
+++ nvim/after/plugin/commands.lua
@@ -49,3 +49,14 @@ vim.api.nvim_create_user_command("ToggleVerboseLogging", function()
 		vim.opt.verbosefile = nil
 	end
 end, {})
+
+vim.api.nvim_create_user_command("Messages", function()
+	local bufnr = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_call(bufnr, function()
+		vim.cmd([[put= execute('messages')]])
+	end)
+	vim.api.nvim_buf_set_options(bufnr, "modifiable", false)
+	vim.cmd.split()
+	local winnr = vim.api.nvim_get_current_win()
+	vim.api.nvim_win_set_buf(winnr, bufnr)
+end, {})
