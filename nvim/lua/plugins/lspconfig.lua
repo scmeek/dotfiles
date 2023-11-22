25f07320d358f8ea0b6b76966e14c5e7ff21cfa4
Sean Meek <sean@seanmeek.com>
2023-11-22 16:28:27 -0700

    nvim - fix autoformat and allow for lsp client overrides

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -214,49 +214,53 @@ return {
 			print("Setting autoformatting to: " .. tostring(format_is_enabled))
 		end, {})
 
-		-- Create an augroup that is used for managing our formatting autocmds.
-		--  We need one augroup per client to make sure that multiple clients
-		--  can attach to the same buffer without interfering with each other.
-		local _augroups = {}
-		local get_augroup = function(client)
-			if not _augroups[client.id] then
-				local group_name = "lsp-format-" .. client.name
-				local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-				_augroups[client.id] = id
-			end
-
-			return _augroups[client.id]
-		end
+		local client_format_overrides = {
+			eslint = function()
+				vim.cmd("silent! EslintFixAll")
+			end,
+			-- If removed, only enable the default formatter if necessary (performance)
+			tsserver = function()
+				vim.cmd("silent! OrganizeImports")
+			end,
+		}
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("lsp-attach-format", { clear = true }),
-			-- This is where we attach the autoformatting for reasonable clients
 			callback = function(args)
-				local client_id = args.data.client_id
-				local client = vim.lsp.get_client_by_id(client_id)
 				local bufnr = args.buf
 
-				if not client.server_capabilities.documentFormattingProvider then
-					return
-				end
-
-				if client.name == "tsserver" then
-					return
-				end
-
-				-- Create an autocmd that will run *before* we save the buffer.
-				--  Run the formatting command for the LSP that has just attached.
 				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = get_augroup(client),
-					buffer = bufnr,
+					group = vim.api.nvim_create_augroup("lsp-auto-format", { clear = true }),
 					callback = function()
 						if not format_is_enabled then
 							return
 						end
+
+						local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+						local default_formatting_capable_client_ids = {}
+						for _, client in ipairs(clients) do
+							if client_format_overrides[client.name] then
+								client_format_overrides[client.name]()
+							else
+								if
+										client.supports_method("textDocument/formatting")
+										or client.supports_method("textDocument/rangeFormatting")
+										or client.server_capabilities.documentFormattingProvider
+								then
+									table.insert(default_formatting_capable_client_ids, client.id)
+								end
+							end
+						end
+
+						if #default_formatting_capable_client_ids == 0 then
+							return
+						end
+
 						vim.lsp.buf.format({
 							async = false,
-							filter = function(c)
-								return c.id == client.id
+							bufnr = bufnr,
+							filter = function(client)
+								return vim.tbl_contains(default_formatting_capable_client_ids, client.id)
 							end,
 						})
 					end,
