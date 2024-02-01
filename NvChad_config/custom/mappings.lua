42d94b3f7651634fae8ac023cf71a407ca9b3287
Sean Meek <sean@seanmeek.com>
2024-01-31 17:58:42 -0700

    nvim - add mappings

--- NvChad_config/custom/mappings.lua
+++ NvChad_config/custom/mappings.lua
@@ -11,11 +11,35 @@ M.general = {
         require("conform").format()
       end,
       "formatting",
-    }
+    },
+
+    ["<leader>sf"] = { "ggVG", "select all in file" },
+    ["Y"] = { "y$", "yank rest of line" },
+    ["<leader>fr"] = {
+      function()
+        require("telescope.builtin").resume()
+      end,
+      "resume find"
+    },
+    ["gp"] = { "`[v`]", "reselect pasted text" },
+    ["gt"] = { ":bn<Cr>", "next buffer" },
+    ["gT"] = { ":bp<CR>", "previous buffer" },
+    ["H"] = { "^", "move to start of line" },  -- corresponding "v" mapping
+    ["L"] = { "$", "move to end of line" },  -- corresponding "v" mapping
 
   },
   v = {
-    [">"] = { ">gv", "indent"},
+    [">"] = { ">gv", "increase indent"},
+    ["<"] = { "<gv", "decrease indent"},
+    ["y"] = { "ygv<Esc>", "yank" },
+    ["Y"] = { "Ygv<Esc>", "yank" },
+    ["H"] = { "^", "move to start of line" },  -- corresponding "n" mapping
+    ["L"] = { "$", "move to end of line" },  -- corresponding "n" mapping
+    ["J"] = { ":m '>+1<CR>gv==kgvo<esc>=kgvo", "move selected up" },
+    ["K"] = { ":m '<-2<CR>gv==jgvo<esc>=jgvo", "move selected down" },
+  },
+  i = {
+    ["<M-BS>"] = { "<Esc>cvb", "backspace entire word" },
   },
 }
 
