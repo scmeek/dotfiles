5ef108309d76a7899bccf867b220a4b79be03d3f
Sean Meek <sean@seanmeek.com>
2024-02-01 13:42:36 -0700

    alacritty, tmux, nvim - fix undercurl

--- NvChad_config/custom/init.lua
+++ NvChad_config/custom/init.lua
@@ -1,2 +1,8 @@
 require("custom.commands")
 require("custom.options")
+
+-- Allow for undercurl diagnostics
+local hl_groups = { "DiagnosticUnderlineError" }
+for _, hl in ipairs(hl_groups) do
+	vim.cmd.highlight(hl .. " gui=undercurl")
+end
