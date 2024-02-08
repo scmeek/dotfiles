d953dec364802da8396b61602e4ad84773cd1157
Sean Meek <sean@seanmeek.com>
2024-02-08 14:02:20 -0700

    nvim - nvim-tree, configure override

--- neovim/custom/configs/overrides/nvimtree.lua
+++ neovim/custom/configs/overrides/nvimtree.lua
@@ -3,14 +3,43 @@ return {
 	opts = {
 		git = {
 			enable = true,
+			show_on_dirs = false,
+		},
+
+		view = {
+			width = 40,
+			preserve_window_proportions = false,
 		},
 
 		renderer = {
-			highlight_git = true,
+			highlight_opened_files = "name",
 			icons = {
+				git_placement = "signcolumn",
 				show = {
+					folder_arrow = false,
 					git = true,
 				},
+			},
+		},
+		diagnostics = {
+			enable = true,
+			severity = {
+				min = vim.diagnostic.severity.WARN,
+				max = vim.diagnostic.severity.ERROR,
+			},
+		},
+		filters = {
+			custom = {
+				"^.git$",
+			},
+		},
+		actions = {
+			change_dir = {
+				enable = false,
+				restrict_above_cwd = true,
+			},
+			open_file = {
+				resize_window = true,
 			},
 		},
 	},
