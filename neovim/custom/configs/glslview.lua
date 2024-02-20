9cc60b7f5ca709b974943718b88580281205f04b
scmeek <sean@seanmeek.com>
2024-02-20 00:23:41 -0700

    nvim - fix glslView-nvim keymap options

--- neovim/custom/configs/glslview.lua
+++ neovim/custom/configs/glslview.lua
@@ -5,10 +5,8 @@ return {
 		{
 			"<leader>gv",
 			":GlslView<CR>",
-			{
-				noremap = true,
-				silent = true,
-			},
+			noremap = true,
+			silent = true,
 			ft = "glsl",
 			desc = "Open in GlslViewer",
 		},
