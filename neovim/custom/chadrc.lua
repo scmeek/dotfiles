de917cc0f9cdf1c9eca92d8ee85d776260ec1317
Sean Meek <sean@seanmeek.com>
2024-02-23 14:44:35 -0700

    nvim - nvchad ui statusline, add percent to cursor_position

--- neovim/custom/chadrc.lua
+++ neovim/custom/chadrc.lua
@@ -4,6 +4,19 @@ local M = {}
 -- Path to overriding theme and highlights files
 local highlights = require("custom.highlights")
 
+-- Requires "minimal" theme as written
+local ui_statusline_cursor_position_add_percent = function(minimum_cursor_position_string)
+	local end_formatting = "%#St_sep_r#█ %#ST_EmptySpace#"
+	local position_to_insert = minimum_cursor_position_string:len() - end_formatting:len()
+	local current_line = vim.fn.line(".", vim.g.statusline_winid)
+	local total_line = vim.fn.line("$", vim.g.statusline_winid)
+	local percent = math.modf((current_line / total_line) * 100) .. tostring("%%")
+	return minimum_cursor_position_string:sub(1, position_to_insert)
+		.. " "
+		.. percent
+		.. minimum_cursor_position_string:sub(position_to_insert + 1)
+end
+
 M.ui = {
 	theme = "tokyonight",
 	theme_toggle = { "tokyonight", "gruvbox_light" },
@@ -24,8 +37,12 @@ M.ui = {
 	},
 
 	statusline = {
-		theme = "minimal",
+		theme = "minimal", -- including custom percent below relies on "minimal"
 		separator_style = "block",
+		overriden_modules = function(modules)
+			-- cursor_position is last_in the table, depends on minimal theme
+			modules[#modules] = ui_statusline_cursor_position_add_percent(modules[#modules])
+		end,
 	},
 }
 
