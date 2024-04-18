c4c8cc8f0377595485fe0b5a4bf0fc02f1e629f7
Sean Meek <sean@seanmeek.com>
2024-04-18 14:44:03 -0600

    hammerspoon - switch from hot reload config detection (symlinks break) to reload shortcut (CTRL+ALT+R)

--- hammerspoon_init.lua
+++ hammerspoon_init.lua
@@ -1,18 +1,3 @@
--- Reload config on ~/.hammerspoon/*.lua file change
-function reloadConfig(files)
-	doReload = false
-	for _, file in pairs(files) do
-		if file:sub(-4) == ".lua" then
-			doReload = true
-		end
-	end
-	if doReload then
-		hs.reload()
-	end
-end
-
-myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-
 local key_app_mapping = {
 	{ "1", "Alacritty" },
 	{ "2", "Safari" },
@@ -20,6 +5,10 @@ local key_app_mapping = {
 	{ "4", "Messages" },
 	{ "5", "Obsidian" },
 }
+
+hs.hotkey.bind({ "alt", "ctrl" }, "R", function()
+	hs.reload()
+end)
 
 local function bind_key_to_launch_app(key, appName)
 	local modifier = "cmd"
@@ -72,3 +61,5 @@ hs.hotkey.bind({ "cmd", "shift" }, "L", function()
 	window_frame.x = screen_frame.w / 2
 	window_frame.y = screen_frame.y
 end)
+
+hs.alert.show("Hammerspoon config loaded")
