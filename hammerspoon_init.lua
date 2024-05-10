1c27d859cf4af332cd5b35d7edf0827a0b7a7862
Sean Meek <sean@seanmeek.com>
2024-05-10 11:53:24 -0600

    hammerspoon - fix window resizing completely

--- hammerspoon_init.lua
+++ hammerspoon_init.lua
@@ -21,6 +21,12 @@ for _, mapping in ipairs(key_app_mapping) do
 	bind_key_to_launch_app(mapping[1], mapping[2])
 end
 
+local function set_frame(window, frame)
+	local duration_seconds = 0.03
+	-- window:setFrameWithWorkarounds(frame, duration_seconds)
+	window:setFrame(frame, duration_seconds)
+end
+
 -- Maximize window
 hs.hotkey.bind({ "cmd", "shift" }, "K", function()
 	local window = hs.window.focusedWindow()
@@ -29,10 +35,9 @@ hs.hotkey.bind({ "cmd", "shift" }, "K", function()
 
 	window_frame.x = screen_frame.x
 	window_frame.y = screen_frame.y
-	window:setFrame(window_frame)
 	window_frame.w = screen_frame.w
 	window_frame.h = screen_frame.h
-	window:setFrame(window_frame)
+	set_frame(window, window_frame)
 end)
 
 -- Set window to left half
@@ -43,10 +48,9 @@ hs.hotkey.bind({ "cmd", "shift" }, "H", function()
 
 	window_frame.w = screen_frame.w / 2
 	window_frame.h = screen_frame.h
-	window:setFrame(window_frame)
 	window_frame.x = screen_frame.x
 	window_frame.y = screen_frame.y
-	window:setFrame(window_frame)
+	set_frame(window, window_frame)
 end)
 
 -- Set window to right half
@@ -57,9 +61,9 @@ hs.hotkey.bind({ "cmd", "shift" }, "L", function()
 
 	window_frame.w = screen_frame.w / 2
 	window_frame.h = screen_frame.h
-	window:setFrame(window_frame)
 	window_frame.x = (screen_frame.x + window_frame.w)
 	window_frame.y = screen_frame.y
+	set_frame(window, window_frame)
 end)
 
 hs.alert.show("Hammerspoon config loaded")
