d25c7b29fbdd76a4a5280e03c816a94196db2470
Sean Meek <sean@seanmeek.com>
2024-08-30 12:36:28 -0600

    wezterm, nvim - improve performance by removing cursor blinking and background opacity and blur

--- wezterm.lua
+++ wezterm.lua
@@ -5,15 +5,10 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.animation_fps = 1
 config.bold_brightens_ansi_colors = true
-config.cursor_blink_ease_in = "Constant"
-config.cursor_blink_ease_out = "Constant"
-config.default_cursor_style = "BlinkingBlock"
 config.default_prog = { "/bin/zsh", "-l", "-c", "tmux new-session -A -s main" }
 config.disable_default_key_bindings = true
 config.force_reverse_video_cursor = true
-config.front_end = "WebGpu"
 config.font = wezterm.font("FiraCode Nerd Font Propo", { weight = "Regular" })
 config.font_rules = {
 	{
@@ -62,13 +57,11 @@ config.keys = {
 	{ key = "q", mods = "SUPER", action = wezterm.action.QuitApplication },
 	{ key = "v", mods = "SUPER", action = wezterm.action.PasteFrom("Clipboard") },
 }
-config.macos_window_background_blur = 7
 config.set_environment_variables = {
 	TERMINFO_DIRS = "~/.local/share/terminfo",
 }
 config.scrollback_lines = 10000
 config.term = "wezterm"
-config.window_background_opacity = 0.85
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 
