373b57a96acd93bf35a5d508e3eda13788804a70
scmeek <sean@seanmeek.com>
2025-04-01 22:53:23 -0600

    wezterm - remove auto maximize on startup

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,6 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
-wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
-end)
-
 local config = {}
 if wezterm.config_builder then
 	config = wezterm.config_builder()
