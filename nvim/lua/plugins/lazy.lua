d2bc5af93662081ff0c4c2fcf106ebdc95701ba3
scmeek <sean@seanmeek.com>
2025-07-10 21:09:12 -0600

    nvim - transparent background

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -1,3 +1,11 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
+    vim.cmd("highlight EndOfBuffer ctermbg=NONE guibg=NONE")
+  end,
+})
+
 return {
   {
     "LazyVim/LazyVim",
