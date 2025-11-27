0eb76e39c2c777e352e5218d70a74fa5d0060533
scmeek <sean@seanmeek.com>
2025-11-27 00:59:12 -0700

    nvim - add avante and enabled with NVIM_ENABLE_AVANTE

--- nvim/lua/extras/init.lua
+++ nvim/lua/extras/init.lua
@@ -26,4 +26,10 @@ if enable_copilot then
   vim.notify("GitHub Copilot enabled", nil, { id = "github_copilot", style = "minimal" })
 end
 
+local enable_avante = os.getenv("NVIM_ENABLE_AVANTE") == "1"
+if enable_avante then
+  table.insert(extras, { import = "lazyvim.plugins.extras.ai.avante" })
+  vim.notify("avante.nvim enabled", nil, { id = "avante", style = "minimal" })
+end
+
 return extras
