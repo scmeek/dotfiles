4863dcb955e45b4cb9553a661767eef6d7fb99cb
scmeek <sean@seanmeek.com>
2026-09-23 11:20:24 -0600

    nvim - improve cross-boundary navigation at outer boundary for tmuxnavigator

--- nvim/lua/plugins/tmuxnavigator.lua
+++ nvim/lua/plugins/tmuxnavigator.lua
@@ -2,9 +2,9 @@ return {
   "christoomey/vim-tmux-navigator",
   event = "VeryLazy",
   keys = {
-    { "<C-h>", "<C-W>h", desc = "Go to previous split" },
-    { "<C-j>", "<C-W>j", desc = "Go down a split" },
-    { "<C-k>", "<C-W>k", desc = "Go up a split" },
-    { "<C-l>", "<C-W>l", desc = "Go to next split" },
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Go to previous split" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Go down a split" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Go up a split" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Go to next split" },
   },
 }
