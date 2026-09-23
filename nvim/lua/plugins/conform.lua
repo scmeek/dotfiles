8d16f8261af0b1282dc59a9118b3f3ee34186063
scmeek <sean@seanmeek.com>
2026-09-23 11:12:56 -0600

    nvim - fix formatters in conform and remove unnecessary code

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,59 +1,14 @@
 return {
   "stevearc/conform.nvim",
-  dependencies = { "mason.nvim" },
-  lazy = true,
-  cmd = "ConformInfo",
-  keys = {
-    {
-      "<leader>cF",
-      function()
-        require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-      end,
-      mode = { "n", "v" },
-      desc = "Format Injected Langs",
-    },
-  },
-  init = function()
-    -- Install the conform formatter on VeryLazy
-    LazyVim.on_very_lazy(function()
-      LazyVim.format.register({
-        name = "conform.nvim",
-        priority = 100,
-        primary = true,
-        format = function(buf)
-          require("conform").format({ bufnr = buf })
-        end,
-        sources = function(buf)
-          local ret = require("conform").list_formatters(buf)
-          ---@param v conform.FormatterInfo
-          return vim.tbl_map(function(v)
-            return v.name
-          end, ret)
-        end,
-      })
-    end)
-  end,
-  opts = function()
-    -- local plugin = require("lazy.core.config").plugins["conform.nvim"]
-    -- if plugin.config ~= M.setup then
-    --   LazyVim.error({
-    --     "Don't set `plugin.config` for `conform.nvim`.\n",
-    --     "This will break **LazyVim** formatting.\n",
-    --     "Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
-    --   }, { title = "LazyVim" })
-    -- end
-
+  opts = function(_, opts)
     local formatters = {
-      c = { "clang-format" },
-      cpp = { "clang-format" },
-      --glsl = { "glsl_analyzer" },
-      --ino = { "clang-format" },
+      c = { "clang_format" },
+      cpp = { "clang_format" },
       lua = { "stylua" },
-      python = { "black", "isort" },
-      rust = { "cargo fmt", lsp_format = "fallback" },
+      python = { "isort", "black" },
+      rust = { "rustfmt", lsp_format = "fallback" },
       sh = { "shfmt" },
       toml = { "taplo" },
-      yaml = { "yamlfmt" },
       zsh = { "shfmt" },
     }
 
@@ -76,38 +31,13 @@ return {
       "yaml",
     }
 
-    for _, filetype in pairs(prettier_filetypes) do
+    for _, filetype in ipairs(prettier_filetypes) do
       formatters[filetype] = { "prettier" }
     end
 
-    ---@type conform.setupOpts
-    local opts = {
-      default_format_opts = {
-        timeout_ms = 3000,
-        async = false, -- not recommended to change
-        quiet = false, -- not recommended to change
-        lsp_format = "fallback", -- not recommended to change
-      },
-      formatters_by_ft = formatters,
-      -- The options you set here will be merged with the builtin formatters.
-      -- You can also define any custom formatters here.
-      ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-      formatters = {
-        injected = { options = { ignore_errors = true } },
-        -- # Example of using dprint only when a dprint.json file is present
-        -- dprint = {
-        --   condition = function(ctx)
-        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-        --   end,
-        -- },
-        --
-        -- # Example of using shfmt with extra args
-        -- shfmt = {
-        --   prepend_args = { "-i", "2", "-ci" },
-        -- },
-      },
-    }
-    return opts
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+    for filetype, names in pairs(formatters) do
+      opts.formatters_by_ft[filetype] = names
+    end
   end,
-  -- config = M.setup,
 }
