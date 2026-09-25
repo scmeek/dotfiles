e9fdd86186a5a348c8020215cd416620f6c9a971
scmeek <sean@seanmeek.com>
2026-09-24 20:54:27 -0600

    nvim - include hidden files by default in search

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,14 @@
 return {
   "snacks.nvim",
   opts = {
+    picker = {
+      -- Alt+h toggles hidden files; Alt+i toggles ignored files.
+      sources = {
+        files = { hidden = true },
+        grep = { hidden = true },
+        grep_word = { hidden = true },
+      },
+    },
     dashboard = {
       preset = {
         header = "Neovim",
